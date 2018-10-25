cbf49b9719971dced760a8bbd80c2567b96fc0fc
Rout222 <routmagno@gmail.com>
2018-10-25 07:37:20 -0300

    teste final

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ base     = sys.argv[1]
 algoritmo     = sys.argv[2]
 training = "./bases/" + base + "-training.csv"
 test	 = "./bases/" + base + "-test.csv"
-print('asdasdas')
+
+print("Lendo os dados de treino")
 with open(training, newline='') as f:
 	xtraining = []
 	ytraining = []
@@ -13,13 +14,13 @@ with open(training, newline='') as f:
 	for row in reader:
 	    xtraining.append(row[:-1])
 	    ytraining.append(row[-1])
-print('asdasdas')
+print("Arrumando os dados de treino")
 for xi, x in enumerate(xtraining):
 	if base == "poker":
 		xtraining[xi] = [x[0], int(x[1]), x[2], int(x[3]), x[4], int(x[5]), x[6], int(x[7]), x[8], int(x[9])]
 	elif base == "velha":
 		xtraining[xi] = [int(x[0]), int(x[1]), int(x[2]), int(x[3]), int(x[4]), int(x[5]), int(x[6]), int(x[7]), int(x[8])]
-print('asdasdas')
+print("lendo os dados de teste")
 with open(test, newline='') as f:
 	xtest = []
 	ytest = []
@@ -27,7 +28,7 @@ with open(test, newline='') as f:
 	for row in reader:
 	    xtest.append(row[:-1])
 	    ytest.append(row[-1])
-print('asdasdas')
+print("Arrumando os dados de teste")
 for xi, x in enumerate(xtest):
 	if base == "poker":
 		xtest[xi] = [x[0], int(x[1]), x[2], int(x[3]), x[4], int(x[5]), x[6], int(x[7]), x[8], int(x[9])]	 
